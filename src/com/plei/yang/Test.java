3a0ce8d6d325cdef7d3d33ba5501fc82d02502d1
pleier <pleiyang@outlook.com>
2016-12-16 14:55:44 +0800

    Initial commit

---
+++ src/com/plei/yang/Test.java
@@ -0,0 +1,12 @@
+package com.plei.yang;
+
+
+
+/**
+ * Created by PLEI on 2016/12/16.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("helloWord!");
+    }
+}
