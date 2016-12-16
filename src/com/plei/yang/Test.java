6bdac712ed174922deb2a41806e06a3789923235
pleier <pleiyang@outlook.com>
2016-12-16 16:22:15 +0800

    这是一个测试

--- src/com/plei/yang/Test.java
+++ src/com/plei/yang/Test.java
@@ -5,9 +5,44 @@ package com.plei.yang;
 /**
  * Created by PLEI on 2016/12/16.
  */
-public class Test {
+public class Test implements TestInterface{
+    @Override
+    public void test() {
+        
+    }
 
-    public static void main(String[] args){
-        System.out.print("helloWord!");
+    @Override
+    public void test1() {
+
+    }
+
+    @Override
+    public void test2() {
+
+    }
+
+    @Override
+    public void test3() {
+
+    }
+
+    @Override
+    public void test4() {
+
+    }
+
+    @Override
+    public void test5() {
+
+    }
+
+    @Override
+    public void test6() {
+
+    }
+
+    @Override
+    public void test7() {
+
     }
 }
---
+++ src/com/plei/yang/TestInterface.java
@@ -0,0 +1,15 @@
+package com.plei.yang;
+
+/**
+ * Created by PLEI on 2016/12/16.
+ */
+public interface TestInterface {
+    public void test();
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
+}
