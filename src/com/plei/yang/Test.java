f7d8724e0407e3e4c8fbb00cd2c6487af354cb5a
pleier <pleiyang@outlook.com>
2016-12-16 15:35:10 +0800

    test

--- src/com/plei/yang/Test.java
+++ src/com/plei/yang/Test.java
@@ -6,7 +6,8 @@ package com.plei.yang;
  * Created by PLEI on 2016/12/16.
  */
 public class Test {
+
     public static void main(String[] args){
-        System.out.print("helloWord!");
+        System.out.print("hcgdfgdfgdelloWord!");
     }
 }
